a590afc6c33e272eff4eec9ade83d4beb82a4415
lbroomf1 <lawrence.broomfield@gmail.com>
2024-03-28 14:01:22 -0400

    initial push

---
+++ TeamPlanning/TeamPlanning/TeamPlanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamPlanningApp.swift
+//  TeamPlanning
+//
+//  Created by LB on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamPlanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
